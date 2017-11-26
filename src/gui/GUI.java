bbeb044362141f4dcc9644e5114185e95940958a
Abdel <Abdel@LAPTOP-UN32RNIM>
2017-11-26 16:52:44 +0200

    Updated

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -7,7 +7,12 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
@@ -55,12 +60,21 @@ public class GUI {
 		frmLogicExpressions.setTitle("Logic expressions");
 		frmLogicExpressions.getContentPane().setForeground(SystemColor.text);
 		frmLogicExpressions.getContentPane().setBackground(SystemColor.inactiveCaptionBorder);
-		frmLogicExpressions.setBounds(100, 100, 803, 857);
+		frmLogicExpressions.setBounds(100, 100, 874, 857);
 		frmLogicExpressions.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JTextArea textArea = new JTextArea();
 		textArea.setFont(new Font("Monospaced", Font.PLAIN, 18));
 		textArea.setEditable(false);
+		
+		textArea.setLineWrap(true);
+	    textArea.setVisible(true);
+
+	    JScrollPane scroll = new JScrollPane (textArea);
+	    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+	    scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+	    frmLogicExpressions.getContentPane().add(scroll);
 		
 		firstExpression = new JTextField();
 		firstExpression.setFont(new Font("Tahoma", Font.PLAIN, 15));
@@ -99,27 +113,33 @@ public class GUI {
 				
 				textArea.setText("");
 				
-				final String expression1 = firstExpression.getText();
-				final int variables1 = Integer.valueOf(firstNumOfVariable.getText());
-				
-				Expression x = new Expression(expression1,variables1);
+				if(firstExpression.getText().isEmpty() || firstNumOfVariable.getText().isEmpty()) {
+					textArea.setText("Invalid inputs\n");
+				}
+				else{
+					final String expression1 = firstExpression.getText();
+					final int variables1 = Integer.valueOf(firstNumOfVariable.getText());
+					Expression x = new Expression(expression1,variables1);
 			
-				final boolean[] arrX = x.generateTruthTable();
+					final boolean[] arrX = x.generateTruthTable();
 				
-				textArea.setText(" The expression : " + expression1 + "\n\n");
-				textArea.append(" Minterms\t Output\n");
+					textArea.setText(" The expression : " + expression1 + "\n\n");
+					textArea.append(" Minterms\t Output\n");
 				
-				for(int i = 0; i < arrX.length ; i++){
-					textArea.append(" m" + i + "\t\t " + arrX[i] + "\n");
+					for(int i = 0; i < arrX.length ; i++){
+						textArea.append(" m" + i + "\t\t " + arrX[i] + "\n");
+					}
+				
+					if (x.isContradiction()){
+						textArea.append(" Result of testing is : Contradiction\n");
+					}
+					else if (x.isTautology()){
+						textArea.append(" Result of testing is : Tautology\n");
+					}
+					else {
+						textArea.append(" Result of testing is : neither tautology nor contradiction\n");
+					}
 				}
-				
-				if (x.isContradiction()){
-					textArea.append("\n Result of testing is : Contradiction");
-				}
-				else if (x.isTautology()){
-					textArea.append("\n Result of testing is : Tautology");
-				}
-				
 			}
 		});
 		
@@ -131,25 +151,33 @@ public class GUI {
 				
 				textArea.setText("");
 				
-				final String expression2 = secondExpression.getText();
-				final int variables2 = Integer.valueOf(secondNumOfVariable.getText());
-				
-				Expression y = new Expression(expression2,variables2);
-				
-				final boolean[] arrY = y.generateTruthTable();
-				
-				textArea.setText(" The expression : " + expression2 + "\n\n");
-				textArea.append(" Minterms\t Output\n");
-				
-				for(int i = 0; i < arrY.length ; i++){
-					textArea.append(" m" + i + "\t\t " + arrY[i] + "\n");
+				if(secondExpression.getText().isEmpty() || secondNumOfVariable.getText().isEmpty()) {
+					textArea.setText("Invalid inputs\n");
 				}
+				else{
+
+					final String expression2 = secondExpression.getText();
+					final int variables2 = Integer.valueOf(secondNumOfVariable.getText());
+					Expression y = new Expression(expression2,variables2);
 				
-				if (y.isContradiction()){
-					textArea.append("\n Result of testing is : Contradiction");
-				}
-				else if (y.isTautology()){
-					textArea.append("\n Result of testing is : Tautology");
+					final boolean[] arrY = y.generateTruthTable();
+				
+					textArea.setText(" The expression : " + expression2 + "\n\n");
+					textArea.append(" Minterms\t Output\n");
+				
+					for(int i = 0; i < arrY.length ; i++){
+						textArea.append(" m" + i + "\t\t " + arrY[i] + "\n");
+					}
+				
+					if (y.isContradiction()){
+						textArea.append(" Result of testing is : Contradiction\n");
+					}
+					else if (y.isTautology()){
+						textArea.append(" Result of testing is : Tautology\n");
+					}
+					else {
+						textArea.append(" Result of testing is : neither tautology nor contradiction\n");
+					}
 				}
 			}
 		});
@@ -162,28 +190,53 @@ public class GUI {
 				
 				textArea.setText("");
 				
-				final String expression1 = firstExpression.getText();
-				final String expression2 = secondExpression.getText();
-				final int variables1 = Integer.valueOf(firstNumOfVariable.getText());
-				final int variables2 = Integer.valueOf(secondNumOfVariable.getText());
-				
-				Expression x = new Expression(expression1,variables1);
-				Expression y = new Expression(expression2,variables2);
-				
-				final boolean[] arrX = x.generateTruthTable();
-				final boolean[] arrY = y.generateTruthTable();
-				
-				textArea.setText(" Comparing : " + expression1 +" and " + expression2 + "\n\n");
-				textArea.append(" Minterms\tFirst output\tSecond output\n");
-				
-				for(int i = 0; i < arrY.length ; i++){
-					textArea.append(" m" + i + "\t\t" + arrX[i] + "\t\t" + arrY[i] + "\n");
-				}
-				if (x.equals(y)){
-					textArea.append("\n The expressions are equivalent");
+				if(secondExpression.getText().isEmpty() || secondNumOfVariable.getText().isEmpty() ||
+						firstExpression.getText().isEmpty() || firstNumOfVariable.getText().isEmpty()){
+					textArea.setText("Invalid inputs\n");
 				}
 				else {
-					textArea.append("\n The expressions aren't equivalent");
+
+					final String expression1 = firstExpression.getText();
+					final String expression2 = secondExpression.getText();
+					final int variables1 = Integer.valueOf(firstNumOfVariable.getText());
+					final int variables2 = Integer.valueOf(secondNumOfVariable.getText());
+					Expression x = new Expression(expression1,variables1);
+					Expression y = new Expression(expression2,variables2);
+				
+					final boolean[] arrX = x.generateTruthTable();
+					final boolean[] arrY = y.generateTruthTable();
+				
+					textArea.setText(" Comparing : " + expression1 +" and " + expression2 + "\n\n");
+					textArea.append(" Minterms\tFirst output\tSecond output\n");
+				
+					for(int i = 0; i < arrY.length ; i++){
+						textArea.append(" m" + i + "\t\t" + arrX[i] + "\t\t" + arrY[i] + "\n");
+					}
+					if (x.equals(y)){
+						textArea.append(" The expressions are equivalent\n");
+					}
+					else {
+						textArea.append(" The expressions aren't equivalent\n");
+					}
+				
+				}
+			}
+		});
+		
+		JButton btnSave = new JButton("Save");
+		btnSave.setForeground(Color.BLUE);
+		btnSave.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				BufferedWriter writer;
+				try {
+					writer = new BufferedWriter(new FileWriter("src/gui/Save", true));
+					textArea.write(writer);
+					writer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 				
 			}
@@ -217,8 +270,11 @@ public class GUI {
 					.addGap(37)
 					.addComponent(btnTestSexondExpression)
 					.addGap(35)
-					.addComponent(btnCompareBoth))
-				.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 787, Short.MAX_VALUE)
+					.addComponent(btnCompareBoth)
+					.addGap(32)
+					.addComponent(btnSave, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(107, Short.MAX_VALUE))
+				.addComponent(scroll)
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -248,12 +304,13 @@ public class GUI {
 							.addComponent(lblNumberOfIp_1))
 						.addComponent(secondNumOfVariable, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE))
 					.addGap(7)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnTestFirstExpression, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnTestSexondExpression, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnCompareBoth, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(btnSave, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnTestFirstExpression, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
+						.addComponent(btnTestSexondExpression, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
+						.addComponent(btnCompareBoth, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE))
 					.addGap(10)
-					.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 538, Short.MAX_VALUE))
+					.addComponent(scroll, GroupLayout.DEFAULT_SIZE, 538, Short.MAX_VALUE))
 		);
 		frmLogicExpressions.getContentPane().setLayout(groupLayout);
 	}
